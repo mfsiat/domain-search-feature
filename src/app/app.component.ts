86753fe4e5afeecff305b965bbb0412c4a8f15a2
Nasirul Islam <nasirul.siat@outlook.com>
2020-10-04 15:11:06 +0600

    solved issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,99 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
+import xml2js from 'xml2js';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  // readonly ROOT_URL = 'https://jsonplaceholder.typicode.com';
-  readonly ROOT_URL =
-    'https://api.namecheap.com/xml.response?ApiUser=softopark&ApiKey=651498aa5e3d40a79826fab2c1cd4e49&UserName=softopark&ClientIp=103.218.26.135&Command=namecheap.domains.check&DomainList=';
-
-  posts: any;
-
+  httpData;
+  // name;
+  // searchparam = 2;
+  // ngOnInit() {
+  //   this.http
+  //     .get('http://jsonplaceholder.typicode.com/users?id=' + this.searchparam)
+  //     .subscribe((data) => this.displaydata(data));
+  // }
+  public xmlItems: any;
   constructor(private http: HttpClient) {}
 
-  getPosts() {
-    this.posts = this.http.get(this.ROOT_URL + 'blogsoftopark.com');
+  ApiUser = 'softopark';
+  domain_name = 'wowweber.com';
+  ApiKey = '651498aa5e3d40a79826fab2c1cd4e49';
+  Command = 'namecheap.domains.check';
+  ClientIp = '103.218.26.135';
+  // ClientIp = '127.0.0.1:4200';
+  // ngOnInit() {
+  //   this.http.get(
+  //     `https://api.namecheap.com/xml.response?ApiUser=softopark&ApiKey=651498aa5e3d40a79826fab2c1cd4e49&UserName=softopark&ClientIp=103.218.26.135&Command=namecheap.domains.check&DomainList=${this.domain_name}`
+  //   );
+  //   // .subscribe((data) => this.displayData(data));
+  // }
+  // displayData(data) {
+  //   this.httpData = data;
+  // }
+  ngOnInit() {
+    this.http
+      .get(
+        `https://api.namecheap.com/xml.response?ApiUser=${this.ApiUser}&ApiKey=${this.ApiKey}&UserName=softopark&ClientIp=${this.ClientIp}&Command=${this.Command}&DomainList=${this.domain_name}`,
+        {
+          headers: new HttpHeaders()
+            .set('Content-Type', 'text/xml')
+            .append('Access-Control-Allow-Methods', 'GET')
+            .append(
+              'Access-Control-Allow-Origin',
+              'https://api.namecheap.com/xml.response'
+            )
+            .append(
+              'Access-Control-Allow-Headers',
+              'Access-Control-Allow-Headers, Access-Control-Allow-Origin, Access-Control-Request-Method'
+            ),
+          responseType: 'text',
+        }
+      )
+      .subscribe((data) => {
+        this.parseXML(data).then((data) => {
+          this.xmlItems = data;
+        });
+      });
+  }
+
+  parseXML(data) {
+    return new Promise((resolve) => {
+      var k: string | number,
+        arr = [],
+        parser = new xml2js.Parser({
+          trim: true,
+          explicitArray: true,
+        });
+      // var parser = new xml2js.Parser({ explicitArray: false });
+      parser.parseString(data, function (err, result) {
+        var obj = result.ApiResponse;
+        for (k in obj.CommandResponse) {
+          var item = obj.CommandResponse[k];
+          arr.push({
+            Name: item.DomainCheckResult[0]['$']['Domain'],
+            Available: item.DomainCheckResult[0]['$']['Available'],
+            PremiumRegistrationPrice:
+              item.DomainCheckResult[0]['$']['PremiumRegistrationPrice'],
+          });
+        }
+        resolve(arr);
+        var obj = result.ApiResponse;
+        for (k in obj.CommandResponse) {
+          var item = obj.CommandResponse[k];
+          arr.push({
+            Name: item.DomainCheckResult[0]['$']['Domain'],
+            Available: item.DomainCheckResult[0]['$']['Available'],
+            PremiumRegistrationPrice:
+              item.DomainCheckResult[0]['$']['PremiumRegistrationPrice'],
+          });
+        }
+        resolve(arr);
+        // resolve(JSON.stringify(arr));
+        console.log(JSON.stringify(item.DomainCheckResult));
+      });
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
-import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-
+import { AppComponent } from './app.component';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
